4580d47d85ad5422a08cafb879da8a8e72bd85b8
Mark Kennard <komcomputers@gmail.com>
2012-05-29 13:16:39 +1200

    Updated build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,17 @@ if [ -z $1 ]; then
 	echo "Kernel will be labelled ($KBUILD_BUILD_VERSION)"
 else
 	if [ $1 = "config" ]; then
+		echo "Executing menuconfig"
 		make menuconfig CROSS_COMPILE=/usr/arm-toolchain/bin/arm-eabi- ARCH=arm
+		exit
+	fi
+	if [ $1 = "saveconfig" ]; then
+		if [ -z $2 ]; then
+			echo "You must specify the destination configuration (XXX_defconfig)"
+		else
+			echo "Saving configuration to arch/arm/$2_defconfig"
+			cat .config | grep "=" > arch/arm/$2_defconfig
+		fi
 		exit
 	fi
 	echo "Setting kernel name to ($1)"
